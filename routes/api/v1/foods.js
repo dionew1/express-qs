f485494e395262fbbd78ab45b5beb48e23c5c9c0
Dione Wilson <dionew1@gmail.com>
2018-02-08 10:54:46 -0700

    Add v1 in api file structure

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -1,8 +1,8 @@
 const environment = process.env.NODE_ENV || 'development'
 const express = require('express');
 const router = express.Router();
-const configuration = require('../../knexfile')[environment]
-const foodsController = require('../../controllers/foodsController')
+const configuration = require('../../../knexfile')[environment]
+const foodsController = require('../../../controllers/foodsController')
 
 router.get('/', foodsController.index)
 router.post('/', foodsController.create)
--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -1,9 +1,9 @@
 const environment = process.env.NODE_ENV || 'development'
 const express = require('express');
 const router = express.Router();
-const configuration = require('../../knexfile')[environment]
-const foodsController = require('../../controllers/foodsController')
-const mealsController = require('../../controllers/mealsController')
+const configuration = require('../../../knexfile')[environment]
+const foodsController = require('../../../controllers/foodsController')
+const mealsController = require('../../../controllers/mealsController')
 
 router.get('/', mealsController.index)
 router.post('/:meal_id/foods/:id', mealsController.create)
