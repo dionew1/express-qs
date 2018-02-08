448292f8ca482621ac66f201b22cc0dd61135a3c
Dione Wilson <dionew1@gmail.com>
2018-02-08 12:12:47 -0700

    Setup npm cors

--- app.js
+++ app.js
@@ -5,29 +5,26 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
+var cors = require('cors');
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 var apiFoods = require('./routes/api/v1/foods');
 var apiMeals = require('./routes/api/v1/meals');
-var cors = require('cors');
 
 var app = express();
-app.use(cors());
 
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
-
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.listen((80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors())
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
