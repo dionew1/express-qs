60196112131c61f2ea07ae6730519de015c4f2d1
Dione Wilson <dionew1@gmail.com>
2018-02-08 22:35:21 -0700

    using express cors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
-var cors = require('cors');
+var cors = require('express-cors');
 
 
 var index = require('./routes/index');
@@ -23,7 +23,9 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(cors())
+app.use(cors({
+  allowedOrigins: [ 'aziobrow.github.io/quantified-self/' ]
+}))
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
